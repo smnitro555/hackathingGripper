1d2bed35a0fcaf7b93883e5c9214ec0387b5ec29
smnitro555 <smnitro555@gmail.com>
2016-10-06 18:52:17 -0400

    testing Ultrasonic and RGB

--- StepperMotorControllerTest/StepperMotorControllerTest.ino
+++ StepperMotorControllerTest/StepperMotorControllerTest.ino
@@ -4,25 +4,28 @@
 // Run a A4998 Stepstick from an Arduino UNOusing AccelStepper
 // Paul Hurley Aug 2015
 
-AccelStepper stepper(1,5,4);//initialise accelstepper for a two wire board, pin 5 step, pin 4 dir
+AccelStepper stepper(1,3,4);//initialise accelstepper for a two wire board, pin 5 step, pin 4 dir
 
 void setup() {
   Serial.begin(9600);
-  pinMode(6,OUTPUT); // Enable
-  digitalWrite(6,LOW); // Set Enable low
+  pinMode(2,OUTPUT); // Enable
+  digitalWrite(2,LOW); // Set Enable low
+  stepper.setMaxSpeed(10);
+  stepper.setSpeed(5);
+  stepper.runSpeed();
 }
 
 void loop() {
-  digitalWrite(6,LOW); // Set Enable low
-  if (stepper.distanceToGo() == 0)
-  {  // Random change to speed, position and acceleration
-    // Make sure we dont get 0 speed or accelerations
-    delay(1000);
-    stepper.moveTo(rand() % 400);
-    stepper.setMaxSpeed((rand() % 400) + 200);
-    stepper.setAcceleration((rand() % 200) + 100);
-  }
-
-Serial.println(stepper.distanceToGo());
-stepper.run();  // Actually makes stepper move
+//  digitalWrite(2,LOW); // Set Enable low
+//  if (stepper.distanceToGo() == 0)
+//  {  // Random change to speed, position and acceleration
+//    // Make sure we dont get 0 speed or accelerations
+//    delay(1000);
+//    stepper.moveTo(100);
+//    stepper.setMaxSpeed(100);
+//    stepper.setAcceleration(10);
+//  }
+//
+//Serial.println(stepper.distanceToGo());
+//stepper.run();  // Actually makes stepper move
 }
