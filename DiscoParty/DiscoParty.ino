04344bdd0ddb851e10d690a643fb2ef248c4ba07
smnitro555 <smnitro555@gmail.com>
2016-10-06 23:31:21 -0400

    Disco Party Frame Work Done

--- DiscoParty/DiscoParty.ino
+++ DiscoParty/DiscoParty.ino
@@ -3,27 +3,42 @@
 
 #define echoPin 12 // Echo Pin
 #define trigPin 13 // Trigger Pin
-// using a 200-step motor (most common)
-// pins used are DIR, STEP, MS1, MS2, MS3 in that order
+    // using a 200-step motor (most common)
+    // pins used are DIR, STEP, MS1, MS2, MS3 in that order
 A4988 stepper(200, 3, 2);
 A4988 pincer_stepper(200, 9, 8);
-// Pins Used for RGB
+    // Pins Used for RGB
 int redPin = 5; //this sets the red led pin
 int greenPin = 6; //this sets the green led pin
 int bluePin = 7; //this sets the blue led pin
-
-// Variables for Arm Control
+    // Force Sensivity Readings
+int fsrAnalogPin1 = 0; // FSR is connected to analog 0
+int fsrAnalogPin2 = 1; // FSR is connected to analog 1
+int fsrAnalogPin3 = 2; // FSR is connected to analog 2
+int fsrReading1;
+int fsrReading2;
+int fsrReading3;
+    // Variables for Arm Control
 int arm1Position = 0;
 int arm2Position = 0;
-// Variables for Ultrasonic
+    // Variables for Ultrasonic
 int maximumRange = 400; // Maximum range needed
 int minimumRange = 0; // Minimum range needed
 long duration, distance; // Duration used to calculate distance
-// Calibration Constants for Distance and RGB
+int mode = 0;
+int modeDrop = 0;
+int triggerDrop = false;
+    // Calibration Constants for Distance and RGB
 int range = 2;
 int tomatoHeight = 20;
 int bookHeight = 10;
-int waterBottleHeight = 15; 
+int waterBottleHeight = 15;
+    // Limit Switch Set-Ups
+int limitPincer = 38;
+int limitUpperBar = 40;
+    // Trigger Input
+int triggerInput = 36;
+
 
 
 void setup() {
@@ -31,15 +46,22 @@ void setup() {
     pinMode(redPin, OUTPUT);
     pinMode(greenPin, OUTPUT);
     pinMode(bluePin, OUTPUT);  
+    // Limit Switch Initialization
+    pinMode(limitPincer, INPUT);
+    pinMode(limitUpperBar, INPUT);
+    // Input Signal Initialization
+    pinMode(triggerInput, INPUT);
     // Set target motor RPM to 1RPM
     stepper.setRPM(200);
     pincer_stepper.setRPM(200);
 //     Set full speed mode (microstepping also works for smoother hand movement
 //    stepper.setMicrostep(1);
     Serial.begin(9600);
+    // Ultrasonic Initialization
     pinMode(trigPin, OUTPUT);
     pinMode(echoPin, INPUT);
     // Code to Reset the Gripper to Full Open Position
+    resetGripper();
 }
 
 void loop() {
@@ -55,13 +77,17 @@ void loop() {
       // Change RGB Color Based on Distance
       if ((distance <= (tomatoHeight + range)) && (distance >= (tomatoHeight - range))) {
         setColor(255, 0, 0);  // red
+        mode = 1; // Tomato Mode
       } else if ((distance <= (bookHeight + range)) && (distance >= (bookHeight - range))) {
         setColor(0, 255, 0);  // green
+        mode = 2; // Book Mode
       } else if ((distance <= (waterBottleHeight + range)) && (distance >= (waterBottleHeight - range))) {
         setColor(0, 0, 255);  // blue
+        mode = 3; // Water Bottle Mode
       } else {
-        setColor(255, 255, 0);  // yellow
+        setColor(80, 0, 80);  // purple
       }
+      // Serial.println("Distance " + String(distance));
 
     // Tell the Motor to Move based on Input
     if (Serial.available() > 0) {
@@ -77,6 +103,45 @@ void loop() {
       stepper.rotate(arm1Position);
       pincer_stepper.rotate(arm2Position);
     }
+
+    if (triggerInput == HIGH && triggerDrop == false) {
+      // Gripper routines
+      if (mode == 0) {
+        // Default Routine
+
+      } else if (mode == 1) {
+        // Tomato Routine
+
+      } else if (mode == 2) {
+        // Book Mode
+
+      } else if (mode == 3) {
+        // Water Bottle Mode
+
+      }
+      triggerDrop = true;
+      modeDrop = mode;
+    }
+
+    if (triggerInput == HIGH && triggerDrop == true) {
+      // Gripper routines
+      if (modeDrop == 0) {
+        // Default Drop Routine Here
+
+      } else if (modeDrop == 1) {
+        // Tomato Drop Routine Here
+
+      } else if (modeDrop == 2) {
+        // Book Drop Routine Here
+
+      } else if (modeDrop == 3) {
+        // Water Bottle Drop Routine Here
+
+      }
+      triggerDrop = true;
+      modeDrop = mode;
+    }
+    resetGripper();
 }
 
 void setColor(int red, int green, int blue)
@@ -90,3 +155,9 @@ void setColor(int red, int green, int blue)
   analogWrite(greenPin, green);
   analogWrite(bluePin, blue);  
 }
+
+void resetGripper() {
+
+}
+
+
--- ultrasonicTest/ultrasonicTest.ino
+++ ultrasonicTest/ultrasonicTest.ino
@@ -15,16 +15,29 @@ Some code and wiring inspired by http://en.wikiversity.org/wiki/User:Dstaub/robo
 #define led 11
 #define led2 10
 
+    // Pins Used for RGB
+int redPin = 5; //this sets the red led pin
+int greenPin = 6; //this sets the green led pin
+int bluePin = 7; //this sets the blue led pin
+    // Calibration Constants for Distance and RGB
+int range = 2;
+int tomatoHeight = 20;
+int bookHeight = 10;
+int waterBottleHeight = 15;
+
 void setup() {
   Serial.begin (9600);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
-  pinMode(led, OUTPUT);
-  pinMode(led2, OUTPUT);
+    // RGB LED Initialization
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT); 
+
 }
 
 void loop() {
-  long duration, distance;
+  int duration, distance;
   distance = 0;
   digitalWrite(trigPin, LOW);  // Added this line
   delayMicroseconds(2); // Added this line
@@ -34,20 +47,36 @@ void loop() {
   digitalWrite(trigPin, LOW);
   duration = pulseIn(echoPin, HIGH);
   distance = (duration/2) / 29.1;
-  if (distance < 4) {  // This is where the LED On/Off happens
-    digitalWrite(led,HIGH); // When the Red condition is met, the Green LED should turn off
-  digitalWrite(led2,LOW);
-}
-  else {
-    digitalWrite(led,LOW);
-    digitalWrite(led2,HIGH);
+
+  // Change RGB Color Based on Distance
+  if ((distance <= (tomatoHeight + range)) && (distance >= (tomatoHeight - range))) {
+    setColor(255, 0, 0);  // red
+
+  } else if ((distance <= (bookHeight + range)) && (distance >= (bookHeight - range))) {
+    setColor(0, 255, 0);  // green
+    Serial.println("Green");
+  } else if ((distance <= (waterBottleHeight + range)) && (distance >= (waterBottleHeight - range))) {
+    setColor(0, 0, 255);  // blue
+
+  } else {
+    setColor(80, 0, 80);  // purple
+    Serial.println("Purple");
   }
-  if (distance >= 800 || distance <= 0){
-    Serial.println("Out of range");
-  }
-  else {
-    Serial.print(distance);
-    Serial.println(" cm");
-  }
+
+  Serial.println("Distance is " + String(distance));
+
+
   delay(500);
 } 
+
+void setColor(int red, int green, int blue)
+{
+  #ifdef COMMON_ANODE
+    red = 255 - red;
+    green = 255 - green;
+    blue = 255 - blue;
+  #endif
+  analogWrite(redPin, red);
+  analogWrite(greenPin, green);
+  analogWrite(bluePin, blue);  
+}
