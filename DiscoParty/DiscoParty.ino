97671de2ec70fe1813f752061f7b34092dac6bb7
smnitro555 <smnitro555@gmail.com>
2016-10-06 21:51:41 -0400

    Added in Ultrasonic Changing RGB LED

--- DiscoParty/DiscoParty.ino
+++ DiscoParty/DiscoParty.ino
@@ -1,27 +1,36 @@
 #include <Arduino.h>
 #include "A4988.h"
 
-
-
 #define echoPin 12 // Echo Pin
 #define trigPin 13 // Trigger Pin
 // using a 200-step motor (most common)
 // pins used are DIR, STEP, MS1, MS2, MS3 in that order
 A4988 stepper(200, 3, 2);
 A4988 pincer_stepper(200, 9, 8);
+// Pins Used for RGB
+int redPin = 5; //this sets the red led pin
+int greenPin = 6; //this sets the green led pin
+int bluePin = 7; //this sets the blue led pin
 
 // Variables for Arm Control
 int arm1Position = 0;
 int arm2Position = 0;
 // Variables for Ultrasonic
-int ops = 0; // Counter so we dont Check Distance Every Cycle
-int ops2check = 50; // Cycles before Distance Refresh
 int maximumRange = 400; // Maximum range needed
 int minimumRange = 0; // Minimum range needed
 long duration, distance; // Duration used to calculate distance
+// Calibration Constants for Distance and RGB
+int range = 2;
+int tomatoHeight = 20;
+int bookHeight = 10;
+int waterBottleHeight = 15; 
 
 
 void setup() {
+    // RGB LED Initialization
+    pinMode(redPin, OUTPUT);
+    pinMode(greenPin, OUTPUT);
+    pinMode(bluePin, OUTPUT);  
     // Set target motor RPM to 1RPM
     stepper.setRPM(200);
     pincer_stepper.setRPM(200);
@@ -30,20 +39,29 @@ void setup() {
     Serial.begin(9600);
     pinMode(trigPin, OUTPUT);
     pinMode(echoPin, INPUT);
+    // Code to Reset the Gripper to Full Open Position
 }
 
 void loop() {
     // Measure Distance from Table
-    if (ops == ops2check) {
-      digitalWrite(trigPin, LOW); 
-      delayMicroseconds(2); 
+      distance = 0;
+      digitalWrite(trigPin, LOW);  // Added this line
+      delayMicroseconds(2); // Added this line
       digitalWrite(trigPin, HIGH);
-      delayMicroseconds(10); 
+      delayMicroseconds(10); // Added this line
       digitalWrite(trigPin, LOW);
       duration = pulseIn(echoPin, HIGH);
-      //Calculate the distance (in cm) based on the speed of sound.
-      distance = duration/58.2;
-    }
+      distance = (duration/2) / 29.1;
+      // Change RGB Color Based on Distance
+      if ((distance <= (tomatoHeight + range)) && (distance >= (tomatoHeight - range))) {
+        setColor(255, 0, 0);  // red
+      } else if ((distance <= (bookHeight + range)) && (distance >= (bookHeight - range))) {
+        setColor(0, 255, 0);  // green
+      } else if ((distance <= (waterBottleHeight + range)) && (distance >= (waterBottleHeight - range))) {
+        setColor(0, 0, 255);  // blue
+      } else {
+        setColor(255, 255, 0);  // yellow
+      }
 
     // Tell the Motor to Move based on Input
     if (Serial.available() > 0) {
@@ -54,14 +72,21 @@ void loop() {
 
       Serial.print("I will move my arms by: (" + String(arm1Position) + ", " + String(arm2Position) + ")");
       Serial.println("");
-      if (ops == 0) {
-        Serial.print("Current Distance from Table is: " + String(distance));
-        Serial.println("")
-        // Change RGB Color Based on Distance
-      }
+      Serial.print("Current Distance from Table is: " + String(distance));
+      Serial.println("");
       stepper.rotate(arm1Position);
       pincer_stepper.rotate(arm2Position);
     }
+}
 
-    ops = ops + 1;
+void setColor(int red, int green, int blue)
+{
+  #ifdef COMMON_ANODE
+    red = 255 - red;
+    green = 255 - green;
+    blue = 255 - blue;
+  #endif
+  analogWrite(redPin, red);
+  analogWrite(greenPin, green);
+  analogWrite(bluePin, blue);  
 }
--- ultransonicTest2/ultransonicTest2.ino
+++ ultransonicTest2/ultransonicTest2.ino
@@ -41,19 +41,19 @@ void loop() {
  
  //Calculate the distance (in cm) based on the speed of sound.
  distance = duration/58.2;
- 
- if (distance >= maximumRange || distance <= minimumRange){
- /* Send a negative number to computer and Turn LED ON 
- to indicate "out of range" */
- Serial.println("-1");
- digitalWrite(LEDPin, HIGH); 
- }
- else {
- /* Send the distance to the computer using Serial protocol, and
- turn LED OFF to indicate successful reading. */
  Serial.println(distance);
- digitalWrite(LEDPin, LOW); 
- }
+// if (distance >= maximumRange || distance <= minimumRange){
+// /* Send a negative number to computer and Turn LED ON 
+// to indicate "out of range" */
+// Serial.println("-1");
+// digitalWrite(LEDPin, HIGH); 
+// }
+// else {
+// /* Send the distance to the computer using Serial protocol, and
+// turn LED OFF to indicate successful reading. */
+// Serial.println(distance);
+// digitalWrite(LEDPin, LOW); 
+// }
  
  //Delay 50ms before next reading.
  delay(50);
